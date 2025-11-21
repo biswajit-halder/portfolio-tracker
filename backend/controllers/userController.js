543256c3ee8dd0996ecbda4bb42a8002fe85a4cf
Biswajit Halder <61617245+biswajit-halder@users.noreply.github.com>
2025-11-21 12:21:48 +0530

    Refactor userController for consistency and readability

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,73 @@
-import User from '../models/userModel.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const genToken = (userId) => {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
+};
 
 export const register = async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
-        const userExists = await User.findOne({ email })
+        const userExists = await User.findOne({ email });
         if (userExists) {
             return res.status(400).json({ message: "User already exists" });
         }
 
         if (password.length < 8) {
-            return res.status(400).json({ message: "Password must be at least 8 charachters" });
+            return res
+                .status(400)
+                .json({ message: "Password must be at least 8 charachters" });
         }
 
-        const salt = bcrypt.genSalt(10)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: genToken(user._id),
+        });
     } catch (error) {
-
+        res.status(500).json({ message: "Server error", error: error.message });
     }
-}
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res
+                .status(500)
+                .json({ message: "Invalid Email or Password" });
+        }
+
+        const isMatched = await bcrypt.compare(password, user.password);
+        if (!isMatched) {
+            return res
+                .status(500)
+                .json({ message: "Invalid Email or Password" });
+        }
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: genToken(user._id),
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
