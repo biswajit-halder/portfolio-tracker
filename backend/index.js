eb6073a5aca7c3bb50a4a2263043e1196bd64aab
Biswajit Halder <61617245+biswajit-halder@users.noreply.github.com>
2025-11-21 12:25:51 +0530

    Refactor index.js for improved routing and syntax

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import { connectDB } from './config/db.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import holdingRoutes from "./routes/holdingRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -9,20 +11,19 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 
 // CONNECT DB
-connectDB()
+connectDB();
 
 // MIDDLEWARES
 app.use(express.json());
 
-
 // ROUTES
+app.use("/api/auth/", userRoutes);
+app.use("/api/holdings", holdingRoutes);
 
-
-app.get('/', (req, res) => {
-    res.send('API WORKING')
-})
-
+app.get("/", (req, res) => {
+    res.send("API WORKING");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
